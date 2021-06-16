380a7ce68ae2f7c641bc265f03ecd3266ca31936
Lin <jamlin@fareastone.com.tw>
2021-06-16 15:16:41 +0800

    add 18~19

---
+++ 19.組件_單文件組件/App.vue
@@ -0,0 +1,24 @@
+
+/* 配置組件的結構 */
+<template>
+   <div>
+       <h2>我是App，我有一台{{car}}</h2>
+       <school/>
+   </div>
+</template>
+
+/* 配置組件數據、交互、事件等等 */
+<script>
+        //引入School組件
+        import School from './components/School.vue'
+    export default {
+        data(){
+            return{
+                car:'馬自達'
+            }
+        },
+        components:{
+            School,
+        }
+    }
+</script>
---
+++ 19.組件_單文件組件/components/School.vue
@@ -0,0 +1,41 @@
+/*
+    較針來講:當前的這個Vue文件，並不是一個組件，而是一個組件所需的所有配置
+*/
+
+/* 配置模板結構 */
+<template>
+    <div>
+        <h2 class="title">學校的名字是: {{name}}</h2>
+        <h2 class="info" @click="showAddress">學校的地址是: {{address}}</h2>′
+    </div>
+</template>
+
+/* 配置組件數據、交互、事件等等 */
+
+<script>
+    //較真來說，此處只是暴露了一個組件的配置，並沒有去創建組件，因為沒有調用Vue.extend
+    export default {
+        // data中存放組件所需數據
+        data(){
+            return {
+                name:'上矽谷',
+                address:'基隆路',
+            }
+        },
+        methods: {
+            showAddress(){
+                alert(this.address)
+            }
+        },
+    }
+</script>
+
+/* 配置組件樣式 */
+<style>
+    .title{
+        background-color: orange;
+    }
+    .info{
+        background-color: pink;
+    }
+</style>
